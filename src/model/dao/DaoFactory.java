5a89634d01703e5de48f23cb1e9c9e75772690de
joaoalflopes <68ronins@gmail.com>
2020-08-07 00:53:52 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
 import model.dao.impl.SellerDaoJDBC;
+import model.database.DB_conector;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB_conector.getConnection());
 	}
 }
